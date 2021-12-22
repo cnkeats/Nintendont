a57baff760aab61b0debd9a6b278b5dd2c5d2231
Jas Laferriere <Fizzi36@gmail.com>
2021-12-22 18:19:47 -0500

    add comment
    hi Nikki

--- common/include/NintendontVersion.h
+++ common/include/NintendontVersion.h
@@ -1,6 +1,8 @@
 #ifndef __NINTENDONT_VERSION_H__
 #define __NINTENDONT_VERSION_H__
 
+// Version number of Nintendont, doesn't show up in the app though, that requires you to push
+// the release tag on github via git push --atomic origin <branch name> <tag>
 #define NIN_VERSION "1.9.3"
 
 #endif
